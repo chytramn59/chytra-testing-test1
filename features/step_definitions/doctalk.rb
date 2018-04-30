2c267911a7e5e185bee23cd24b848d7fa0f9405e
chytramn59 <cnagaraju@qwinix.io>
2018-04-30 12:59:49 +0530

    reindent

--- features/step_definitions/doctalk.rb
+++ features/step_definitions/doctalk.rb
@@ -4,17 +4,17 @@ Given("I am in doctalk home page") do
 end
 
 When("I click on signup button") do
-	sleep 2
-   @browser.find_element(xpath: "//div[2]/ul/li[1]/form/input").displayed?
-   sleep 2
-    @browser.find_element(xpath: "//div[2]/ul/li[1]/form/input").click
+  sleep 2
+  @browser.find_element(xpath: "//div[2]/ul/li[1]/form/input").displayed?
+  sleep 2
+  @browser.find_element(xpath: "//div[2]/ul/li[1]/form/input").click
 end
 
 Then("I should get singup form for new sign-in") do
-	sleep 3
-if  @browser.find_element(id: "popup_modal").displayed?
-	puts "login popup displayed"
-end
+  sleep 3
+  if  @browser.find_element(id: "popup_modal").displayed?
+    puts "login popup displayed"
+  end
 end
 Given("I am on singup form") do
   @browser.find_element(id: "popup_modal").displayed?
@@ -22,18 +22,18 @@ end
 
 When("I enter all the details") do
   @browser.find_element(id: "user_name").send_keys "chytra"
-  @browser.find_element(id: "user_email").send_keys "chytramn88@gmail.com"
+  @browser.find_element(id: "user_email").send_keys "chytramn85@gmail.com"
   @browser.find_element(id: "user_password").send_keys "Chytra123!"
-   @browser.find_element(id: "user_password_confirmation").send_keys "Chytra123!"
-   sleep 3
-    @browser.find_element(name: "commit").click
+  @browser.find_element(id: "user_password_confirmation").send_keys "Chytra123!"
+  sleep 3
+  @browser.find_element(name: "commit").click
 
 end
 
 Then("I should see signup succesfully") do 
-	sleep 5
- if @browser.find_element(xpath: "//div[2]/ul/li[2]/form/input[2]").displayed?
- 	puts "sucessfully logged out"
-end
+  sleep 5
+  if @browser.find_element(xpath: "//div[2]/ul/li[2]/form/input[2]").displayed?
+    puts "sucessfully logged out"
+  end
 end
 
