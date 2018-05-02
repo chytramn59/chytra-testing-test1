4b7fd079126e8e7b16a02c8c52e9b66cb291b2b7
chytramn59 <cnagaraju@qwinix.io>
2018-05-02 11:58:23 +0530

    edited

--- features/doctalk.feature
+++ features/doctalk.feature
@@ -1,23 +1,16 @@
-@signup_new_user
+@User_signup_and_logout
 Feature: As a end user
 I should be able open singup page 
 and I should able create the new user and signup succesfully.
 
-
-@sign_up
-Scenario:new_sign_up
-Given I am in doctalk home page
-When I click on signup button
-Then I should get singup form for new sign-in
-
-@valid_sign_up
-Scenario:sign-up
-Given I am on singup form
-When I enter all the details
-Then I should see signup succesfullys
+@smoke
+Scenario: trying to singup with valid credentials
+Given I am on doctalk home page
+When I enter all the vaild details
+Then I should see "Signed up Successfully!"
 
 @signout
-Scenario:sign-out
-Given I am on home page
+Scenario: logging out of the appliaction
+Given I am on dashboard page
 When I click on signout button
-Then I should see signouted succesfully
+Then I should see message "Logged Out Successfully!"
--- features/step_definitions/doctalk.rb
+++ features/step_definitions/doctalk.rb
@@ -1,55 +1,49 @@
 
-Given("I am in doctalk home page") do
-  @browser.navigate.to "https://doctalkweb.herokuapp.com/"
+Given("I am on doctalk home page") do
+   @browser.navigate.to "https://doctalkweb.herokuapp.com/"
 end
 
-When("I click on signup button") do
-  sleep 2
+When("I enter all the vaild details") do
+   sleep 2
   @browser.find_element(xpath: "//div[2]/ul/li[1]/form/input").displayed?
   sleep 2
   @browser.find_element(xpath: "//div[2]/ul/li[1]/form/input").click
-end
-
-Then("I should get singup form for new sign-in") do
   sleep 3
   if  @browser.find_element(id: "popup_modal").displayed?
     puts "login popup displayed"
   end
-end
-Given("I am on singup form") do
-  @browser.find_element(id: "popup_modal").displayed?
-end
-
-When("I enter all the details") do
-  @browser.find_element(id: "user_name").send_keys "chytra"
-  @browser.find_element(id: "user_email").send_keys "chytramn33@gmail.com"
+   @browser.find_element(id: "user_name").send_keys "chytra"
+  @browser.find_element(id: "user_email").send_keys "chytramn65699@gmail.com"
   @browser.find_element(id: "user_password").send_keys "Chytra123!"
   @browser.find_element(id: "user_password_confirmation").send_keys "Chytra123!"
   sleep 3
   @browser.find_element(name: "commit").click
-
 end
 
-# Then("I should see signup succesfully") do 
-#   sleep 5
-#   if @browser.find_element(xpath: "//div[2]/ul/li[2]/form/input[2]").displayed?  
-#     puts "sucessfully logged out"
-#   end
-# end
-
-Then("I should see signup succesfullys") do
- @browser.find_element(id: "flash")
+Then("I should see {string}") do |var|
+  var1=@browser.find_element(xpath: "//*[@id='flash']/div/p").text 
+  if var1==var 
+  puts "#{var1}" 
+  else 
+  raise.Expection.new "#{var1} not found" 
+  end
 end
 
-Given("I am on home page") do
-  @browser.find_element(id: "content")
+
+Given("I am on dashboard page") do
+   @browser.find_element(id: "content")
 end
 
 When("I click on signout button") do
   @browser.find_element(xpath: "//div[2]/ul/li[2]/form/input[2]").displayed? 
-   @browser.find_element(xpath: "//div[2]/ul/li[2]/form/input[2]").click
+  @browser.find_element(xpath: "//div[2]/ul/li[2]/form/input[2]").click
 end
 
-Then("I should see signouted succesfully") do
-  puts "sucessfully logged out"
+Then("I should see message {string}") do |varr|
+ var1=@browser.find_element(xpath: "//*[@id='flash']/div/p").text 
+  if var1==varr 
+  puts "#{var1}" 
+  else 
+  raise.Expection.new "#{var1} not found" 
+  end
 end
